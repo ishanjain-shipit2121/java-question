c3051a36d9242bfc7e82cf1ac3bb090e9389084f
ishanjain-shipit2121 <ishanjain6144@gmail.com>
2026-05-15 19:47:34 +0530

    new changes

---
+++ intro question/assignmentoperatoer.java
@@ -0,0 +1,8 @@
+public class assignmentoperatoer {
+  public static void main(String args[]) {
+    int a = 10;
+    a = a + 10;
+    System.out.println(a);
+    
+  }
+}
---
+++ intro question/operators.java
@@ -0,0 +1,10 @@
+
+public class operators {
+  
+
+  public static void main(String arga[]) {
+    int A = 10;
+    int B = 5;
+    System.out.println("add = "  + (A+B));
+  }
+}
