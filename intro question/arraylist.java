bf5e29e2acc65832cd63879888ac5e3e2f500da8
ishanjain-shipit2121 <ishanjain6144@gmail.com>
2026-06-05 23:34:01 +0530

    Create arraylist.java

---
+++ intro question/arraylist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class arraylist {
+  public static void main(String args[]) {
+    
+ArrayList<Integer> list = new ArrayList<>();
+ArrayList<String> list2 = new ArrayList<>();
+ArrayList<Boolean> list3 = new ArrayList<>();
+  }
+
+  
+}
