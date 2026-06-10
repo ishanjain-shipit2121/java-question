017ed146b1e3b71612512a821c5b649b8efbab99
ishanjain-shipit2121 <ishanjain6144@gmail.com>
2026-06-10 23:44:31 +0530

    Create QueueDemo.java

---
+++ intro question/QueueDemo.java
@@ -0,0 +1,18 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
